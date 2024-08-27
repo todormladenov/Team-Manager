e5b26c7b45e4caa83937617aef4a94fb393f0bb4
Todor Mladenov <toshko958@gmail.com>
2024-08-27 12:58:13 +0200

    Add Member interface

---
+++ client/src/types/members.ts
@@ -0,0 +1,13 @@
+export interface Member {
+    _ownerId: string,
+    teamId: string,
+    status: string,
+    _createdOn: number,
+    _updatedOn: number,
+    _id: string,
+    user: {
+        email: string,
+        username: string,
+        _id: string
+    }
+}
--- client/src/types/teams.ts
+++ client/src/types/teams.ts
@@ -1,3 +1,5 @@
+import { Member } from "./members"
+
 export interface Teams {
     _ownerId: string,
     name: string,
@@ -5,4 +7,8 @@ export interface Teams {
     description: string,
     _createdOn: number,
     _id: string
+}
+
+export interface TeamWithMembers extends Teams {
+    members: Member[]
 }

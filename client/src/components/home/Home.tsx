b1a7a95e4f4e20cf542e0722cca054a825b409df
Todor Mladenov <toshko958@gmail.com>
2024-08-29 16:21:13 +0200

    Add auth context

--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -1,17 +1,20 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
 export default function Home() {
+    const { isAuth } = useContext(AuthContext);
     return (
         <section id="home">
             <article className="hero layout">
                 <img src="./../../assets/team.png" className="left-col pad-med" />
-                    <div className="pad-med tm-hero-col">
-                        <h2>Welcome to Team Manager!</h2>
-                        <p>Want to organize your peers? Create and manage a team for free.</p>
-                        <p>Looking for a team to join? Browse our communities and find like-minded people!</p>
-                        <Link to="/auth/register" className="action cta">Sign Up Now</Link>
-                        <Link to="/teams" className="action cta">Browse Teams</Link>
-                    </div>
+                <div className="pad-med tm-hero-col">
+                    <h2>Welcome to Team Manager!</h2>
+                    <p>Want to organize your peers? Create and manage a team for free.</p>
+                    <p>Looking for a team to join? Browse our communities and find like-minded people!</p>
+                    {!isAuth && <Link to="/auth/register" className="action cta">Sign Up Now</Link>}
+                    <Link to="/teams" className="action cta">Browse Teams</Link>
+                </div>
             </article>
         </section>
     );
--- client/src/components/teams/teams-catalog/TeamsCatalog.tsx
+++ client/src/components/teams/teams-catalog/TeamsCatalog.tsx
@@ -2,9 +2,12 @@ import { Link } from "react-router-dom";
 import TeamLayout from "./TeamLayout";
 import { useTeams } from "../../../hooks/useTeams";
 import { Teams } from "../../../types/teams";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 export default function TeamsCatalog() {
     const teams: Teams[] = useTeams();
+    const { isAuth } = useContext(AuthContext);
 
     return (
         <section id="browse">
@@ -12,10 +15,11 @@ export default function TeamsCatalog() {
             <article className="pad-med">
                 <h1>Team Browser</h1>
             </article>
-
-            <article className="layout narrow">
-                <div className="pad-small"><Link to="#" className="action cta">Create Team</Link></div>
-            </article>
+            {isAuth &&
+                <article className="layout narrow">
+                    <div className="pad-small"><Link to="#" className="action cta">Create Team</Link></div>
+                </article>
+            }
 
             {teams.map(team => <TeamLayout team={team} key={team._id} />)}
 

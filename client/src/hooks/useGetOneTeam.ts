34f9ced775d7801439ab3e0f75712d5a04abec10
Todor Mladenov <toshko958@gmail.com>
2024-08-31 15:35:08 +0200

    Refactor useGetOneTeam hook to use useReducer for state management

--- client/src/hooks/useGetOneTeam.ts
+++ client/src/hooks/useGetOneTeam.ts
@@ -1,10 +1,52 @@
-import { useEffect, useState } from "react"
+import { useEffect, useReducer } from "react"
 import { TeamWithMembers } from "../types/teams"
 import { getOneTeamById } from "../services/teamsAPI";
 import { getAllTeamMembers, getAllTeamPendingMembers } from "../services/membersAPI";
 
+enum ActionType {
+    GET = 'GET',
+    REMOVE_MEMBER = 'REMOVE_MEMBER',
+    REMOVE_PENDING = 'REMOVE_PENDING',
+}
+
+interface StateAction {
+    type: ActionType;
+    payload: TeamWithMembers;
+    memberId?: string
+}
+
+const initialState: TeamWithMembers = {
+    _id: '',
+    name: '',
+    logoUrl: '',
+    description: '',
+    _ownerId: '',
+    _createdOn: 0,
+    members: [],
+    pendingMembers: []
+};
+
+function teamReducer(state: TeamWithMembers, action: StateAction): TeamWithMembers {
+    switch (action.type) {
+        case ActionType.GET:
+            return { ...state, ...action.payload };
+        case ActionType.REMOVE_MEMBER:
+            return {
+                ...state,
+                members: action.payload.members.filter(member => member._id !== action.memberId)
+            }
+        case ActionType.REMOVE_PENDING:
+            return {
+                ...state,
+                pendingMembers: action.payload.pendingMembers.filter(member => member._id !== action.memberId)
+            }
+        default:
+            return state;
+    }
+}
+
 export const useGetOneTeam = (teamId: string | undefined) => {
-    const [team, setTeam] = useState<TeamWithMembers | undefined>();
+    const [team, dispatch] = useReducer(teamReducer, initialState);
 
     useEffect(() => {
         (async () => {
@@ -13,9 +55,16 @@ export const useGetOneTeam = (teamId: string | undefined) => {
                 getAllTeamMembers(teamId),
                 getAllTeamPendingMembers(teamId)]);
 
-            setTeam(() => ({ ...team, members, pendingMembers: pending }));
+            dispatch({
+                type: ActionType.GET,
+                payload: { ...team, members, pendingMembers: pending }
+            });
         })()
     }, [teamId]);
 
-    return team
+    const changeTeamState = (type: ActionType, memberId?: string) => {
+        dispatch({ type, payload: team, memberId })
+    }
+
+    return { team, changeTeamState, ActionType }
 }

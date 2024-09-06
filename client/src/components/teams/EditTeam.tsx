a8af22538086cd8a6ca7e14cb4dd89a68e08c354
Todor Mladenov <toshko958@gmail.com>
2024-09-06 14:36:31 +0200

    Add edit team functionality

--- client/src/components/teams/EditTeam.tsx
+++ client/src/components/teams/EditTeam.tsx
@@ -1,15 +1,67 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate, useParams } from "react-router-dom";
+import { schema, TeamFormValues } from "./create-team/createTeamValidation";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useGetOneTeam } from "../../hooks/useGetOneTeam";
+import { updateTeam } from "../../services/teamsAPI";
+
 export default function EditTeam() {
+    const { teamId } = useParams();
+    const navigator = useNavigate();
+    const [responseError, setResponseError] = useState<string | undefined>(undefined);
+    const { team } = useGetOneTeam(teamId);
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<TeamFormValues>({
+        resolver: yupResolver(schema),
+        values: {
+            name: team.name,
+            logoUrl: team.logoUrl,
+            description: team.description
+        }
+    });
+
+    const editHandler = handleSubmit(async (data) => {
+        setResponseError(undefined);
+
+        try {
+            if (!teamId) {
+                throw new Error('Invalid Team ID');
+            }
+
+            await updateTeam(teamId, data);
+            navigator(`/teams/details/${teamId}`)
+        } catch (error) {
+            if (error instanceof Error) {
+                setResponseError(error.message);
+            } else {
+                setResponseError('Failed to update team');
+            }
+        }
+    });
+    
     return (
         <section id="edit">
             <article className="narrow">
                 <header className="pad-med">
                     <h1>Edit Team</h1>
                 </header>
-                <form id="edit-form" className="main-form pad-large">
-                    {/* <div className="error">Error message.</div> */}
-                    <label>Team name: <input type="text" name="name" /></label>
-                    <label>Logo URL: <input type="text" name="logoUrl" /></label>
-                    <label>Description: <textarea name="description"></textarea></label>
+                <form id="edit-form" className="main-form pad-large" onSubmit={editHandler}>
+                    {responseError && <div className="error">{responseError}</div>}
+
+                    <label>Team name: <input type="text" {...register('name')} /></label>
+                    {errors.name?.message && <div className="error">{errors.name?.message}</div>}
+
+                    <label>Logo URL: <input type="text" {...register('logoUrl')} /></label>
+                    {errors.logoUrl?.message && <div className="error">{errors.logoUrl?.message}</div>}
+
+                    <label>Description: <textarea {...register('description')}></textarea></label>
+                    {errors.description?.message && <div className="error">{errors.description?.message}</div>}
+
                     <input className="action cta" type="submit" value="Save Changes" />
                 </form>
             </article>
--- client/src/services/teamsAPI.ts
+++ client/src/services/teamsAPI.ts
@@ -1,6 +1,6 @@
 import { TeamFormValues } from "../components/teams/create-team/createTeamValidation";
 import { Teams } from "../types/teams";
-import { get, post } from "./api";
+import { get, post, put } from "./api";
 
 const BASE_URL = 'http://localhost:3030/data/teams';
 
@@ -9,3 +9,6 @@ export const getAllTeams = (): Promise<Teams[]> => get<Teams[]>(BASE_URL);
 export const getOneTeamById = (teamId: string | undefined): Promise<Teams> => get<Teams>(`${BASE_URL}/${teamId}`);
 
 export const createTeam = (data: TeamFormValues): Promise<Teams> => post<Teams>(BASE_URL, data);
+
+export const updateTeam = (teamId: string, data: TeamFormValues): Promise<Teams> =>
+    put<Teams>(`${BASE_URL}/${teamId}`, data);
